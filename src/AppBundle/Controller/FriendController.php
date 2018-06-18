77a0b4e258eb4c8c5095ce3b7c5afc51939e3322
Unknown <yann.miloux@gmail.com>
2018-06-19 00:32:50 +0200

    User \\ Register with FB done

--- src/AppBundle/Controller/FriendController.php
+++ src/AppBundle/Controller/FriendController.php
@@ -18,44 +18,46 @@ class FriendController extends BaseController{
     /**
      * @Rest\Post("/friends/request", name="friend_request")
      * @Rest\View(StatusCode = 200)
-     * @ParamConverter("friend", converter="fos_rest.request_body")
      */
-    public function addFriendRequest(Friend $friend, ConstraintViolationList $violations ){
-        if (count($violations)) {
+    public function addFriendRequest(Request $request ){
+        /*if (count($violations)) {
 			$message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
 			foreach ($violations as $violation) {
 				$message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
 			}
 			throw new ResourceValidationException($message);
-        }
+        }*/
+
+        $status =  $request->get('friend_status');
+        $adder = $this->getUserRepository()->find($request->get('id'));
+
+        $userManager = $this->get('fos_user.user_manager');
+        $added = $userManager->findUserByUsername($request->get('username'));
 
         $em = $this->getDoctrine()->getManager();
 
         $checkFriend = $this->getDoctrine()->getRepository(Friend::class)
-        ->checkUsers($friend->getUsers());
+        ->checkUsers([$adder, $added]);
 
         if($checkFriend){
             return 'Already in friend';
         }else{
             $newFriend = new Friend();
-            $newFriend->setStatus($friend->getStatus());
-
-            foreach($friend->getUsers() as $user){
-                $userToAdd = $this->getUserRepository()->find($user->getId());
-                $newFriend->addUser($userToAdd);
-            }
+            $newFriend->setStatus($status);
+            $newFriend->addUser($adder);
+            $newFriend->addUser($added);
             
             // SEND MAIL TO SECOND USER
-            $this->friendRequest($this, $newFriend->getUsers()[1]->getEmail(), $newFriend->getUsers()[0]);
+            $this->friendRequestEmail($this, $added->getEmail(), $adder);
 
             $em->persist($newFriend);
             $em->flush();
 
-            return $newFriend;
+            return $newFriend->getUsers()[0];
         }
     }
 
-    public function friendRequest($controller,$sendTo, $userRequest){
+    public function friendRequestEmail($controller,$sendTo, $userRequest){
         $message = (new \Swift_Message('Sports \\ Friend Request'))
         ->setFrom('sportsesgi@gmail.com')
         ->setTo($sendTo)
